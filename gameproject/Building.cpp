744d6b0e2fd1d0f962019d3249cd6efd0066c34e
Josque <joost@wagensveld.eu>
2015-11-09 15:38:58 +0100

    verslag en GDD toegevoegd...

--- gameproject/Building.cpp
+++ gameproject/Building.cpp
@@ -3,7 +3,7 @@
 #include "Building.h"
 
 Building::Building(textureID id, const textureHolder& textures, sf::Vector2f pos, sf::Color color) :
-	PlayerObject(id, textures, pos, color)
+	PlayerObject{ id, textures, pos, color }
 {
 	numFrames = 1;
 }
